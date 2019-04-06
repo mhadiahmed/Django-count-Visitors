4d493bbd2ae7a874e6a07b1d7ac0d5faaa52c98b
Mhadi Ahmed <mhadiahmed@users.noreply.github.com>
2019-04-06 08:50:49 -0400

    Update sample-idea-code.py

--- sample-idea-code.py
+++ sample-idea-code.py
@@ -9,3 +9,14 @@
 #     blog_object.blog_views=blog_object.blog_views+1
 #     blog_object.save()
 #     #your code
+
+# get one value from the models
+# var = [] #empty list
+# query = Newsletter.Objects.all().values('email')
+# s = list(query)
+# print the s value to check it if list or not after doing that loop throe the items to get only the text value
+# for i in s:
+#     print(i)
+#     for f in i.values():
+#         print(f)
+#         g.append(f)
